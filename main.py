68d32471565282b254491fcbabe242006e9f19d6
marcodn-tuidi <marco.dinardo@tuidi.it>
2024-10-21 20:14:16 +0200

    pre-commit hooks bug fixed and logging

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import logging
+
 import yaml
 from src.personality_types.data_processor import DataProcessor
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 volume_path = "/Volumes/marvelous_dev_ops/personality_types/data/"
 file_name = "people_personality_types.csv"
@@ -14,16 +21,31 @@ print(yaml.dump(config, default_flow_style=False))
 
 # Initialize DataProcessor
 data_processor = DataProcessor(data_path, True, config)
+logger.info("DataProcessor initialized.")
 
 # Preprocess the data
 data_processor.preprocess_data()
 preprocessor = data_processor.preprocessor
+logger.info("Data preprocessed.")
 
 # Split the data
 X_train, X_test, y_train, y_test = data_processor.split_data()
+logger.info("Data split into training and test sets.")
+
+logger.debug(
+    f"Training set shape: {X_train.shape}, Test set shape: {X_test.shape}"
+)
+print(f"Training set shape: {X_train.shape}, Test set shape: {X_test.shape}")
 
 X_train_preprocessed = preprocessor.fit_transform(X_train)
 X_test_preprocessed = preprocessor.fit_transform(X_test)
+logger.info("Data processed.")
 
-print(f"X_train_preprocessed shape: {X_train_preprocessed.shape}")
-print(f"X_test_preprocessed shape: {X_test_preprocessed.shape}")
+print(
+    f"""Processed training set shape: {X_train_preprocessed.shape} Processed
+    test set shape: {X_test_preprocessed.shape}"""
+)
+logger.debug(
+    f"""Processed training set shape: {X_train_preprocessed.shape} Processed
+    test set shape: {X_test_preprocessed.shape}"""
+)
