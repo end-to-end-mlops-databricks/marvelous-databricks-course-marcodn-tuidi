362a502d3bc603a05912bdb0878be062c8d9c1d9
marcodn-tuidi <marco.dinardo@tuidi.it>
2024-11-03 12:49:07 +0100

    add notebook to create serving endppoint for basic model

---
+++ notebooks/model_serving.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+# MAGIC %pip install /Volumes/marvelous_dev_ops/personality_types/package/mlops_with_databricks-0.0.1-py3-none-any.whl --force-reinstall
+
+# COMMAND ----------
+
+# MAGIC %restart_python
+
+# COMMAND ----------
+from databricks.sdk import WorkspaceClient
+from personality_types.config import ProjectConfig
+from personality_types.registered_model_serving import create_model_serving
+
+# COMMAND ----------
+workspace = WorkspaceClient()
+config = ProjectConfig.from_yaml(config_path="../project_config.yml")
+
+# COMMAND ----------
+create_model_serving(
+    config=config,
+    workspace=workspace,
+    endpoint_name="personality-types-basic-model-serving",
+    model_name="personality_model_basic",
+    model_version=11,
+)
