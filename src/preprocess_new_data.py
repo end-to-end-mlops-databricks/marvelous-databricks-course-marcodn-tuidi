f1d50ce5d52d59d9acf93438698693abfa642ced
marcodn-tuidi <marco.dinardo@tuidi.it>
2024-11-15 21:22:28 +0100

    add parser and root argument

--- src/preprocess_new_data.py
+++ src/preprocess_new_data.py
@@ -1,3 +1,5 @@
+import argparse
+
 from databricks.sdk import WorkspaceClient
 from pyspark.sql import SparkSession
 
@@ -5,11 +7,23 @@ from personality_types.config import ProjectConfig
 from personality_types.new_data_preprocessor import NewDataProcessor
 from personality_types.utils.logger_utils import set_logger
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--root_path",
+    action="store",
+    default=None,
+    type=str,
+    required=True,
+)
+args = parser.parse_args()
+
 spark = SparkSession.builder.getOrCreate()
 
 logger = set_logger()
 
-config = ProjectConfig.from_yaml(config_path="./project_config.yml")
+config = ProjectConfig.from_yaml(
+    config_path=f"{args.root_path}/project_config.yml"
+)
 logger.info("Configuration loaded.")
 
 workspace = WorkspaceClient()
